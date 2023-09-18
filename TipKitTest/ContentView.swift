9bf61540de5caf90275ca122b6c68f4e58f07450
An Byungwook <abw2619@icloud.com>
2023-09-18 12:32:37 +0900

    Initial Commit

---
+++ TipKitTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TipKitTest
+//
+//  Created by 안병욱의 mac on 2023/09/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TipKitTest/TipKitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipKitTestApp.swift
+//  TipKitTest
+//
+//  Created by 안병욱의 mac on 2023/09/18.
+//
+
+import SwiftUI
+
+@main
+struct TipKitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
